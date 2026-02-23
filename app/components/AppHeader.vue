e4c95682ac92eac30611cbeb0c733f49cce499ac
Patrick Hofmann <patrick@hofmann.eco>
2026-02-23 21:29:23 +0100

    docs: fix header — always show 🦍 AppLogo instead of plain text title

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -17,28 +17,8 @@ const { header } = useAppConfig()
       class="w-full"
     />
 
-    <template
-      v-if="header?.logo?.dark || header?.logo?.light || header?.title"
-      #title
-    >
-      <UColorModeImage
-        v-if="header?.logo?.dark || header?.logo?.light"
-        :light="header?.logo?.light!"
-        :dark="header?.logo?.dark!"
-        :alt="header?.logo?.alt"
-        class="h-6 w-auto shrink-0"
-      />
-
-      <span v-else-if="header?.title">
-        {{ header.title }}
-      </span>
-    </template>
-
-    <template
-      v-else
-      #left
-    >
-      <NuxtLink :to="header?.to || '/'">
+    <template #left>
+      <NuxtLink :to="header?.to || '/'" class="flex items-center">
         <AppLogo class="w-auto h-6 shrink-0" />
       </NuxtLink>
     </template>
